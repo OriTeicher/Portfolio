7c31b84f517a19cca49148fde2731d8ba8687e9d
OriTeicher <oriteicherudemy@gmail.com>
2023-09-15 04:03:55 +0300

    Some minor UI changes in navbar

--- src/components/IntroPreview.tsx
+++ src/components/IntroPreview.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SupermanImg from '../assets/photos/super-ori.png'
 import AnimeImg from '../assets/photos/ori-anime.png'
 import RegularImg from '../assets/photos/regular ori.jpg'
@@ -7,18 +7,6 @@ import CaptainImg from '../assets/photos/ori-captain.png'
 import PinzziPhoto from '../assets/photos/oris-dog.jpg'
 
 export default function IntroPreview() {
-    const [selectedImg, setSelectedImg] = useState(RegularImg)
-    const [isAnimationOn, setIsAnimationOn] = useState(false)
-
-    const handleSelectedBtn = (selectedImgSrc: string) => {
-        if (selectedImgSrc === selectedImg) return
-        setIsAnimationOn(true)
-        setTimeout(() => {
-            setSelectedImg(selectedImgSrc)
-            setIsAnimationOn(false)
-        }, 250)
-    }
-
     const imageOptions = [
         { imgSrc: RegularImg, label: 'Just Ori...' },
         { imgSrc: PinzziPhoto, label: 'My Beloved Dog' },
@@ -28,22 +16,52 @@ export default function IntroPreview() {
         { imgSrc: CaptainImg, label: 'Captain Kfar Sava' }
     ]
 
+    const [selectedImg, setSelectedImg] = useState(RegularImg)
+    const [isOnStartAnimationOn, setIsOnStartAnimationOn] = useState(false)
+    const [isAnimationOn, setIsAnimationOn] = useState(false)
+
+    useEffect(() => {
+        setIsOnStartAnimationOn(true)
+        const timer = setTimeout(() => {
+            setSelectedImg(RegularImg)
+            setIsOnStartAnimationOn(false)
+        }, 500)
+        return () => clearTimeout(timer)
+    }, [])
+
+    const handleSelectedBtn = (selectedImgSrc: string) => {
+        if (selectedImgSrc === selectedImg) return
+        setIsAnimationOn(true)
+        const timer = setTimeout(() => {
+            setSelectedImg(selectedImgSrc)
+            setIsAnimationOn(false)
+        }, 240)
+        return () => clearTimeout(timer)
+    }
+
     return (
-        <article className="container intro-container flex align-center column">
+        <article className={`container intro-container flex align-center column ${isOnStartAnimationOn ? 'fade-in-slide-in' : 'show'}`}>
             <div className={`img-container ${isAnimationOn ? 'flipped' : ''}`}>
                 <img src={selectedImg} alt="🖼️" className="main-photo" />
             </div>
             <div className="main-header-container">
-                <h1 className="main-header">
-                    <span>Nice to meet ya!</span> <span>I'm Ori Teicher</span>
-                </h1>
+                <div className="main-header">
+                    <p>Nice to meet ya!</p>
+                    <p>
+                        I'm <span>Ori Teicher</span>
+                    </p>
+                </div>
                 <h2 className="role-header">Full Stack / Frontend Developer</h2>
             </div>
             <div className="flex column text-align-center">
                 <p className="description">You are more than welcome to see me as...</p>
                 <div className="btns-container grid justify-center align-center">
                     {imageOptions.map((option) => (
-                        <button key={option.label} onClick={() => handleSelectedBtn(option.imgSrc)} className={selectedImg === option.imgSrc ? 'selected' : ''}>
+                        <button
+                            key={option.label}
+                            onClick={() => handleSelectedBtn(option.imgSrc)}
+                            className={selectedImg === option.imgSrc ? 'selected' : ''}
+                        >
                             {option.label}
                         </button>
                     ))}

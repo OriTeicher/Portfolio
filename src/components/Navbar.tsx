04660b4b53553a99541c457bbfa0b5239d9ff2bd
OriTeicher <oriteicherudemy@gmail.com>
2023-09-15 14:05:01 +0300

    start animation fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react'
 import ContactIcons from './ContactIcons'
 import NavList from './NavList'
 
 export default function Navbar() {
+    const [isAnimationOn, setIsAnimationOn] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsAnimationOn(false)
+        }, 700)
+    }, [])
+
     return (
-        <section className="navbar-container flex align-center justify-center space-evenly">
+        <section className={`navbar-container flex align-center justify-center space-evenly ${isAnimationOn ? 'hide slide-in-right' : 'show'}`}>
             <h3 className="logo">
                 Ori<span>Dev</span>
             </h3>
